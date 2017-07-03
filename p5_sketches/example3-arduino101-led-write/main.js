e674133925f34bb095435d1825842c8ee7f4184e
Yining Shi <ys2189@nyu.edu>
2017-07-02 22:23:09 -0400

    updating led example

--- p5_sketches/example3-arduino101-led-write/main.js
+++ p5_sketches/example3-arduino101-led-write/main.js
@@ -1,9 +1,9 @@
 const serviceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214"; // lowercase hex characters e.g. '00001234-0000-1000-8000-00805f9b34fb'
 const name = 'LEDCB';
 
-var ledCharacteristic;
+var ledCharacteristic, input, connectButton, writeButton;
 
-function connect() {
+function connectBLE() {
   let options = {
     filters: [{
       services: [serviceUuid],
@@ -27,52 +27,15 @@ function connect() {
     })
     .then(characteristics => {
       console.log('Got LED Characteristic');
-
       ledCharacteristic = characteristics[0];
-
-      // write 0/1 to led every 1 second
-      // let byte = 0;
-      // let bufferToSend = Uint8Array.of(byte);
-      // console.log('bufferToSend: ', bufferToSend);
-
-      // setInterval(function() {
-      //    byte ^= 1;
-      //    bufferToSend = Uint8Array.of(byte);
-      //    ledCharacteristic.writeValue(bufferToSend);
-      // }, 1000);
-
     })
     .catch(error => {
       console.log('Argh! ' + error);
     });
-  }
-
-
-var input, connectButton, writeButton, intro;
-
-function setup() {
-  noCanvas();
-
-  connectButton = createButton('connect');
-  connectButton.position(20, 15);
-  connectButton.mousePressed(connect);
-
-  input = createInput();
-  input.position(20, 85);
-
-  writeButton = createButton('submit');
-  writeButton.position(input.x + input.width, 85);
-  writeButton.mousePressed(writeToBle);
-
-  intro = createElement('h2', 'Write 1/0 to turn on/off the on-board LED on Arduino 101:');
-  intro.position(20, 35);
-
-  textAlign(CENTER);
-  textSize(50);
 }
 
-function writeToBle() {
-  var inputValue = input.value();
+function writeToBLE() {
+  let inputValue = document.getElementById('input-box').value;
 
   let bufferToSend = Uint8Array.of(inputValue);
   ledCharacteristic.writeValue(bufferToSend);
